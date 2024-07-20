c723473c50a51b67353370c25c901d8934108f39
Hannes Ellinger <10652476+el-han@users.noreply.github.com>
2024-07-20 18:34:11 +0200

    Add support for C/C++ (#24)
    This commit adds C/C++ template support.
    
    ---------
    
    Co-authored-by: deathaxe <deathaxe82@googlemail.com>

---
+++ Syntaxes/C++/syntax_test_scopes.jinja.cpp
@@ -0,0 +1,58 @@
+// SYNTAX TEST "C++ (Jinja).sublime-syntax"
+
+#include <cstdint>
+
+  {% if true %}
+//^^^^^^^^^^^^^ meta.embedded.statement.jinja
+//^^ punctuation.section.embedded.begin.jinja
+//   ^^ keyword.control.conditional.if.jinja
+//      ^^^^ constant.language.boolean.jinja
+//           ^^ punctuation.section.embedded.end.jinja
+
+  {% else %}
+//^^^^^^^^^^ meta.embedded.statement.jinja
+//^^ punctuation.section.embedded.begin.jinja
+//   ^^^^ keyword.control.conditional.else.jinja
+//        ^^ punctuation.section.embedded.end.jinja
+
+  {% endif %}
+//^^^^^^^^^^^ meta.embedded.statement.jinja
+//^^ punctuation.section.embedded.begin.jinja
+//   ^^^^^ keyword.other.endtag.jinja
+//         ^^ punctuation.section.embedded.end.jinja
+
+int foo(int i) {
+
+  char *msg = "My {{message}}!"
+//            ^^^^^^^^^^^^^^^^^ string.quoted.double.c
+//                ^^^^^^^^^^^ meta.embedded.expression.jinja
+//                ^^ punctuation.section.embedded.begin.jinja
+//                  ^^^^^^^ source.jinja.embedded variable.other.jinja
+//                         ^^ punctuation.section.embedded.end.jinja
+
+  {% if true %}
+//^^^^^^^^^^^^^ meta.function.c++ meta.block.c++ meta.embedded.statement.jinja
+//^^ punctuation.section.embedded.begin.jinja
+//   ^^ keyword.control.conditional.if.jinja
+//      ^^^^ constant.language.boolean.jinja
+//           ^^ punctuation.section.embedded.end.jinja
+
+    i++;
+
+  {% else %}
+//^^^^^^^^^^ meta.function.c++ meta.block.c++ meta.embedded.statement.jinja
+//^^ punctuation.section.embedded.begin.jinja
+//   ^^^^ keyword.control.conditional.else.jinja
+//        ^^ punctuation.section.embedded.end.jinja
+
+    i--;
+
+  {% endif %}
+//^^^^^^^^^^^ meta.function.c++ meta.block.c++ meta.embedded.statement.jinja
+//^^ punctuation.section.embedded.begin.jinja
+//   ^^^^^ keyword.other.endtag.jinja
+//         ^^ punctuation.section.embedded.end.jinja
+
+    return 0;
+
+}
